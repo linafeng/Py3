ef80c82c57fb0d3228ddb0077f5db37f8666ed0b
lina.feng <lina.feng@gentinghk.com>
2019-07-26 18:06:53 +0800

    数据类型 运算符

---
+++ 1_data_type/data_type.py
@@ -0,0 +1,15 @@
+## -*- coding: utf-8 -*-
+#整形
+first_num = 1
+print(first_num)
+print(type(first_num))
+#浮点型
+second_float_num = 1.2
+print(second_float_num)
+print(type(second_float_num))
+#布尔值
+third_boolean = False
+print(third_boolean)
+print(type(third_boolean))
+
+
